3d47877acb196c53e30de982ae07da9d462d1bae
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-03-26 18:54:49 -0400

    [variable] printing data types to the console

--- variable/Program.cs
+++ variable/Program.cs
@@ -7,7 +7,17 @@ namespace variable
         static void Main(string[] args)
         {
             byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Steven";
+            bool isWorking = true;
             Console.WriteLine(number);
+            System.Console.WriteLine(count);
+            System.Console.WriteLine(totalPrice);
+            System.Console.WriteLine(character);
+            System.Console.WriteLine(firstName);
+            System.Console.WriteLine(isWorking);
         }
     }
 }
