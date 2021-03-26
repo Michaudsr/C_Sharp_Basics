0b79d389aa8bd52a82540a2ef924fbb3bde18162
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-03-26 19:04:22 -0400

    [variable] changed all variable datatypes to var, let does not work

--- variable/Program.cs
+++ variable/Program.cs
@@ -6,18 +6,18 @@ namespace variable
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Steven";
-            bool isWorking = true;
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Steven";
+            var isWorking = true;
             Console.WriteLine(number);
-            System.Console.WriteLine(count);
-            System.Console.WriteLine(totalPrice);
-            System.Console.WriteLine(character);
-            System.Console.WriteLine(firstName);
-            System.Console.WriteLine(isWorking);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
         }
     }
 }
