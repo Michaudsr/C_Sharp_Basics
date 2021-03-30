c1ca51f477ca4dec0668fb723e0a36a0a64e14fb
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-03-29 22:21:53 -0400

    [TypeConversion] cast int to a byte

--- TypeConversion/Program.cs
+++ TypeConversion/Program.cs
@@ -6,9 +6,9 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
-            byte b = 1;
-            int i = b;
-            System.Console.WriteLine(i);
+            int i = 1;
+            byte b = (byte)i;
+            Console.WriteLine(b);
         }
     }
 }
