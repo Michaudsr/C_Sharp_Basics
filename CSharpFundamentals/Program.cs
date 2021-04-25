a372310d3d2ca0d53fe5ac1c987e6dbed4de30c3
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-04-25 16:43:18 -0400

    [CSharpFundamentals] write method to console using Parse  method

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -52,6 +52,9 @@ namespace CSharpFundamentals
             var methodId = 3;
             Console.WriteLine((ShippingMethod)methodId);
             Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
         }
     }
 }
