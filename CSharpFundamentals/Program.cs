a85d968115f359960f1c226a98cfed9cddbed070
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-04-25 15:52:23 -0400

    [CSharpFundamentals] printing a array of strings to the console

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -20,6 +20,11 @@ namespace CSharpFundamentals
             Console.WriteLine(flags[1]);
             Console.WriteLine(flags[2]);
 
+            var names = new string[3] {"Jack", "John", "Mary"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+
         }
     }
 }
