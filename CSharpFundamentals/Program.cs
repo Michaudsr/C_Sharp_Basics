191ea0591b226987625b8e6b695b9f3c31142b0a
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-04-25 16:12:09 -0400

    [CSharpFundamentals] write var formattedNames to console using Join method

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -8,22 +8,31 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var numbers = new int[3];
-            numbers[0] = 1;
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
+            // var numbers = new int[3];
+            // numbers[0] = 1;
+            // Console.WriteLine(numbers[0]);
+            // Console.WriteLine(numbers[1]);
+            // Console.WriteLine(numbers[2]);
 
-            var flags = new bool[3];
-            flags[0] = true;
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
+            // var flags = new bool[3];
+            // flags[0] = true;
+            // Console.WriteLine(flags[0]);
+            // Console.WriteLine(flags[1]);
+            // Console.WriteLine(flags[2]);
 
-            var names = new string[3] {"Jack", "John", "Mary"};
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[1]);
-            Console.WriteLine(names[2]);
+            // var names = new string[3] {"Jack", "John", "Mary"};
+            // Console.WriteLine(names[0]);
+            // Console.WriteLine(names[1]);
+            // Console.WriteLine(names[2]);
+            
+            var firstName = "Mosh";
+            var lastName = "Hamedani";
+
+            var fullName = firstName + " " + lastName;
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+            var names = new string[3] {"John", "Jack", "Mary"};
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
 
         }
     }
