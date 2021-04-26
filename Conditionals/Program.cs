e066028359ecbe438cd027e1082e39a5a83c8f0a
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-04-26 18:45:48 -0400

    [Conditionals] examples of a switch statement

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Conditionals
 {
-    class Program
+   class Program
     {
         static void Main(string[] args)
         {
-            bool isGoldCustomer = true;
+            var season = Season.Autumn;
+
+            switch (season){
+                case Season.Autumn:
+                case Season.Summer:
+                    Console.WriteLine("We've got promotion");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+
+            }
+
+
+
+
+
+
+
+            // bool isGoldCustomer = true;
             // float price;
             // if (isGoldCustomer){
             //     price = 19.95f;
@@ -15,8 +34,8 @@ namespace Conditionals
             //     price = 29.95f;
             // }
 
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
-            Console.WriteLine(price);
+            // float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            // Console.WriteLine(price);
             // int hour = 10;
             // if (hour > 0 && hour < 12)
             // {
