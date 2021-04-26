5999a53ba8406a2a4feb33c0f7abb07fc0b1076a
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-04-26 18:16:36 -0400

    [CSharpFundamentals] create a person object and use MakeOld function to Increment person Age

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,71 +1,71 @@
-﻿using System;
-// using CSharpFundamentals.Math;
+﻿// using System;
+// // using CSharpFundamentals.Math;
 
 
-namespace CSharpFundamentals
-{
-    // public enum ShippingMethod 
-    // {
-    //     RegularAirMail = 1,
-    //     RegissteredAirMail = 2,
-    //     Express = 3
-    // }
-      class Program
-    {
-        static void Main(string[] args)
-        {
-            // var numbers = new int[3];
-            // numbers[0] = 1;
-            // Console.WriteLine(numbers[0]);
-            // Console.WriteLine(numbers[1]);
-            // Console.WriteLine(numbers[2]);
+// namespace CSharpFundamentals
+// {
+//     // public enum ShippingMethod 
+//     // {
+//     //     RegularAirMail = 1,
+//     //     RegissteredAirMail = 2,
+//     //     Express = 3
+//     // }
+//       class Program
+//     {
+//         // static void Main(string[] args)
+//         {
+//             // var numbers = new int[3];
+//             // numbers[0] = 1;
+//             // Console.WriteLine(numbers[0]);
+//             // Console.WriteLine(numbers[1]);
+//             // Console.WriteLine(numbers[2]);
 
-            // var flags = new bool[3];
-            // flags[0] = true;
-            // Console.WriteLine(flags[0]);
-            // Console.WriteLine(flags[1]);
-            // Console.WriteLine(flags[2]);
+//             // var flags = new bool[3];
+//             // flags[0] = true;
+//             // Console.WriteLine(flags[0]);
+//             // Console.WriteLine(flags[1]);
+//             // Console.WriteLine(flags[2]);
 
-            // var names = new string[3] {"Jack", "John", "Mary"};
-            // Console.WriteLine(names[0]);
-            // Console.WriteLine(names[1]);
-            // Console.WriteLine(names[2]);
+//             // var names = new string[3] {"Jack", "John", "Mary"};
+//             // Console.WriteLine(names[0]);
+//             // Console.WriteLine(names[1]);
+//             // Console.WriteLine(names[2]);
             
-            // var firstName = "Mosh";
-            // var lastName = "Hamedani";
+//             // var firstName = "Mosh";
+//             // var lastName = "Hamedani";
 
-            // var fullName = firstName + " " + lastName;
-            // var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
-            // var names = new string[3] {"John", "Jack", "Mary"};
-            // var formattedNames = string.Join(",", names);
-            // Console.WriteLine(formattedNames);
+//             // var fullName = firstName + " " + lastName;
+//             // var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+//             // var names = new string[3] {"John", "Jack", "Mary"};
+//             // var formattedNames = string.Join(",", names);
+//             // Console.WriteLine(formattedNames);
 
-            // var text = @"Hi John
-            // Look into the following paths
-            // c:\folder1\folder2
-            // c:\folder3\folder4";
-            // Console.WriteLine(text);
+//             // var text = @"Hi John
+//             // Look into the following paths
+//             // c:\folder1\folder2
+//             // c:\folder3\folder4";
+//             // Console.WriteLine(text);
             
-            // var method = ShippingMethod.Express;
-            // Console.WriteLine((int)method);
+//             // var method = ShippingMethod.Express;
+//             // Console.WriteLine((int)method);
 
-            // var methodId = 3;
-            // Console.WriteLine((ShippingMethod)methodId);
-            // Console.WriteLine(method.ToString());
+//             // var methodId = 3;
+//             // Console.WriteLine((ShippingMethod)methodId);
+//             // Console.WriteLine(method.ToString());
 
-            // var methodName = "Express";
-            // var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+//             // var methodName = "Express";
+//             // var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
 
-            var a = 10;
-            var b = a;
-            b++;
-            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+//             // var a = 10;
+//             // var b = a;
+//             // b++;
+//             // Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
 
-            var array1 = new int[3] {1, 2, 3};
-            var array2 = array1;
-            array2[0] = 0;
-            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+//             // var array1 = new int[3] {1, 2, 3};
+//             // var array2 = array1;
+//             // array2[0] = 0;
+//             // Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
 
-        }
-    }
-}
+//         }
+//     }
+// }
