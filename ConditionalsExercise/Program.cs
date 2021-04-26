9c63da13f55b6e8ad68054a6d8e0e525f08aad63
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-04-26 19:18:12 -0400

    [ConditionalsExercise] my solution to #1 43.Excerises under Control Flow

---
+++ ConditionalsExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConditionalsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 to 10");
+            var number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(IsNumValid(number) ? "Valid" : "Invalid");
+        }
+        static bool IsNumValid(int number){
+            return number >= 1 && number <= 10;
+        }
+    }
+}
