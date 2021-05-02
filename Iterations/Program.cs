096c211dbd35afe1e7d51f088a498d69a3942881
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-05-02 16:16:00 -0400

    [Iterations] my solution for how many numbers are divisble by 3 with no remind, 1-100

--- Iterations/Program.cs
+++ Iterations/Program.cs
@@ -4,7 +4,7 @@ namespace Iterations
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main()
         // {
         //    for (var i =1; i <= 10; i++)
         //    {
@@ -69,14 +69,27 @@ namespace Iterations
 
         //     }
         // }
+        // {
+        //     var random = new Random();
+        //     const int passwordLength = 10;
+        //     var buffer = new char[passwordLength];
+        //     for (var i = 0; i < passwordLength; i++)
+        //         buffer[i] = (char)('a' + random.Next(0, 26));
+        //     var password = new string(buffer);
+        //     Console.WriteLine(password);
+            
+        // }
+        // 1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. 
+        // Display the count on the console
         {
-            var random = new Random();
-            const int passwordLength = 10;
-            var buffer = new char[passwordLength];
-            for (var i = 0; i < passwordLength; i++)
-                buffer[i] = (char)('a' + random.Next(0, 26));
-            var password = new string(buffer);
-            Console.WriteLine(password);
+            
+            var count = 0;
+            for (var i = 1; i <= 100; i++)
+                if (i % 3 == 0) count++;
+                Console.WriteLine(count);
+
+            return count;
+            
             
         }
     }
