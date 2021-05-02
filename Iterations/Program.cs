391e6ebb8a7461f64ade60074c5c4d65a22a93dd
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-05-02 14:09:50 -0400

    [Iterations] example of foreach loop in C#

--- Iterations/Program.cs
+++ Iterations/Program.cs
@@ -5,21 +5,34 @@ namespace Iterations
     class Program
     {
         static void Main(string[] args)
+        // {
+        //    for (var i =1; i <= 10; i++)
+        //    {
+        //        if (i%2 == 0)
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+        //    } 
+        //    for (var i = 10; i >= 1; i--)
+        //    {
+        //        if (i%2 == 0)
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+        //    }
+        // }
         {
-           for (var i =1; i <= 10; i++)
-           {
-               if (i%2 == 0)
-               {
-                   Console.WriteLine(i);
-               }
-           } 
-           for (var i = 10; i >= 1; i--)
-           {
-               if (i%2 == 0)
-               {
-                   Console.WriteLine(i);
-               }
-           }
+            var name = "John Smith";
+
+            // for (var i = 0; i < name.Length; i++)
+            // {
+            //     Console.WriteLine(name[i]);
+            // }
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
         }
     }
 }
