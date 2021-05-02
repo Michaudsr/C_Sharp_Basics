c5a0e19818030b566c0f20bae4900aea8163aa44
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-05-01 21:14:23 -0400

    [ConditionalsExercise] my solution for #3 to determine if a image is landscape or portrait

--- ConditionalsExercise/Program.cs
+++ ConditionalsExercise/Program.cs
@@ -5,6 +5,7 @@ namespace ConditionalsExercise
     class Program
     {
         static void Main()
+       
         // {
         //     // 1- Write a program and ask the user to enter a number. 
         //     // The number should be between 1 to 10.
@@ -19,26 +20,46 @@ namespace ConditionalsExercise
         // static bool IsNumValid(int number){
         //     return number >= 1 && number <= 10;
         // }
+        // {
+        // //    2- Write a program which takes two numbers from the console and displays the maximum of the two. 
+        //     Console.WriteLine("Type in two numbers to be compared and find the maximum");
+        //     Console.WriteLine("First number: ");
+        //     var firstNum = Convert.ToInt32(Console.ReadLine());
+        //     Console.WriteLine("Second Number: ");
+        //     var secondNum = Convert.ToInt32(Console.ReadLine());
+
+        //     if (firstNum == secondNum)
+            
+        //         Console.WriteLine("These numbers are the same");
+            
+        //     else 
+        //         Console.WriteLine("Between {0} and {1}, the maximum is: {2}", firstNum, secondNum, getMaximum(firstNum, secondNum));
+            
+        //     static int getMaximum(int firstNum, int secondNum)
+        //     {
+        //         return (firstNum > secondNum) ? firstNum : secondNum;
+        //     }
+
+        // }
+        
         {
-        //    2- Write a program which takes two numbers from the console and displays the maximum of the two. 
-            Console.WriteLine("Type in two numbers to be compared and find the maximum");
-            Console.WriteLine("First number: ");
-            var firstNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Second Number: ");
-            var secondNum = Convert.ToInt32(Console.ReadLine());
-
-            if (firstNum == secondNum)
+            // 3- Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait.
+            Console.WriteLine("Enter the width and height of an image to determine if it is landscape or portrait.");
+            Console.ReadLine();
+            Console.WriteLine("Width: ");
+            var width = Convert.ToInt32(Console.ReadLine());
             
-                Console.WriteLine("These numbers are the same");
+            Console.WriteLine("Height: ");
+            var height = Convert.ToInt32(Console.ReadLine());
             
-            else 
-                Console.WriteLine("Between {0} and {1}, the maximum is: {2}", firstNum, secondNum, getMaximum(firstNum, secondNum));
-            
-            static int getMaximum(int firstNum, int secondNum)
-            {
-                return (firstNum > secondNum) ? firstNum : secondNum;
-            }
-
+            if(width == height)
+                Console.WriteLine("this image is a 1:1 ratio");
+            if(width > height)
+                Console.WriteLine("Landscape");
+            if(width < height)
+                Console.WriteLine("Portrait");
         }
+      
     }
 }
+
