050ecc79ffc1ee5390344564a3cb2aff82f738a8
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-05-01 21:48:13 -0400

    [ConditionalsExercise] my solution for #4 to determine carSpeed and speedLimit

--- ConditionalsExercise/Program.cs
+++ ConditionalsExercise/Program.cs
@@ -42,24 +42,55 @@ namespace ConditionalsExercise
 
         // }
         
+        // {
+        //     // 3- Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait.
+        //     Console.WriteLine("Enter the width and height of an image to determine if it is landscape or portrait.");
+        //     Console.ReadLine();
+        //     Console.WriteLine("Width: ");
+        //     var width = Convert.ToInt32(Console.ReadLine());
+            
+        //     Console.WriteLine("Height: ");
+        //     var height = Convert.ToInt32(Console.ReadLine());
+            
+        //     if(width == height)
+        //         Console.WriteLine("this image is a 1:1 ratio");
+        //     if(width > height)
+        //         Console.WriteLine("Landscape");
+        //     if(width < height)
+        //         Console.WriteLine("Portrait");
+        // }
         {
-            // 3- Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait.
-            Console.WriteLine("Enter the width and height of an image to determine if it is landscape or portrait.");
-            Console.ReadLine();
-            Console.WriteLine("Width: ");
-            var width = Convert.ToInt32(Console.ReadLine());
+            // 4- Your job is to write a program for a speed camera.
+            // For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic.
+            // Write a program that asks the user to enter the speed limit. 
+            // Once set, the program asks for the speed of a car. 
+            // If the user enters a value less than the speed limit, program should display Ok on the console.
+            // If the value is above the speed limit, the program should calculate the number of demerit points. 
+            // For every 5km/hr above the speed limit, 1 demerit points should be incurred and displayed on the console. 
+            // If the number of demerit points is above 12, the program should display License Suspended.
+            Console.WriteLine("Enter the speed limit: ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the speed of the car: ");
+            var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (carSpeed > speedLimit)
+            {
+                var demeritPoints = 0;
+                while(carSpeed > speedLimit)
+                {
+                    carSpeed -= 5;
+                    demeritPoints ++;
+                }
+                if (demeritPoints > 12)
+                    Console.WriteLine("License Suspended");
             
-            Console.WriteLine("Height: ");
-            var height = Convert.ToInt32(Console.ReadLine());
-            
-            if(width == height)
-                Console.WriteLine("this image is a 1:1 ratio");
-            if(width > height)
-                Console.WriteLine("Landscape");
-            if(width < height)
-                Console.WriteLine("Portrait");
+            }
+            else
+            {
+                Console.WriteLine("OK");
+            }
         }
-      
     }
 }
 
