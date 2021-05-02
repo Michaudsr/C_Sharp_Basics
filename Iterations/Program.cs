f0b4176e44f43de9ec614c17a61a31542ecae6de
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-05-02 14:35:43 -0400

    [Iterations] example of creating a string of characters for a password using a for loop and the random method

--- Iterations/Program.cs
+++ Iterations/Program.cs
@@ -54,20 +54,30 @@ namespace Iterations
         // }
 
         // }
+        // {
+        //     while (true)
+        //     {
+        //         Console.Write("Type your name: ");
+        //         var input = Console.ReadLine();
+
+        //         if (!string.IsNullOrWhiteSpace(input))
+        //         {
+        //             Console.WriteLine("@Echo: " + input);
+        //             continue;
+        //         }
+        //         break;
+
+        //     }
+        // }
         {
-            while (true)
-            {
-                Console.Write("Type your name: ");
-                var input = Console.ReadLine();
-
-                if (!string.IsNullOrWhiteSpace(input))
-                {
-                    Console.WriteLine("@Echo: " + input);
-                    continue;
-                }
-                break;
-
-            }
+            var random = new Random();
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            var password = new string(buffer);
+            Console.WriteLine(password);
+            
         }
     }
 }
