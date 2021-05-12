fcc26d6f76b47976dc99c8eb9fb86cf45e9c715e
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2021-05-12 19:38:54 -0400

    [ArraysAndLists] solution for exercise 5 write a program where user supplies list of integers and returns the 3 smallest

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -137,30 +137,70 @@ namespace ArraysAndLists
         // 4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. 
         // The list of numbers may include duplicates. 
         // Display the unique numbers that the user has entered.
+        // {
+        //     var numbers = new List<int>();
+        //     while(true)
+        //     {
+        //         Console.WriteLine("Enter a number(or 'Quit' to Exit): ");
+        //         var input = Console.ReadLine();
+
+        //         if(input.ToLower() == "Quit")
+        //             break;
+                
+        //         numbers.Add(Convert.ToInt32(input));
+
+        //     }
+
+        //     var uniques = new List<int>();
+        //     foreach(var number in numbers)
+        //     {
+        //         if(!uniques.Contains(number))
+        //             uniques.Add(number);
+        //     }
+        //     Console.WriteLine("Unique numbers: ");
+        //     foreach(var number in uniques)
+        //         Console.WriteLine(number);
+            
+        // }
+        // 5- Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). 
+        // If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.
         {
-            var numbers = new List<int>();
+            string[] elements;
             while(true)
             {
-                Console.WriteLine("Enter a number(or 'Quit' to Exit): ");
+                Console.WriteLine("Enter a list of comma-separated numbers: ");
                 var input = Console.ReadLine();
 
-                if(input.ToLower() == "Quit")
-                    break;
-                
-                numbers.Add(Convert.ToInt32(input));
-
+                if(!String.IsNullOrWhiteSpace(input))
+                {
+                    elements = input.Split(',');
+                    if(elements.Length >= 5)
+                        break;
+                }
+                Console.WriteLine("Invalid List");
             }
+            var numbers = new List<int>();
+            foreach(var number in elements)
+                numbers.Add(Convert.ToInt32(number));
 
-            var uniques = new List<int>();
-            foreach(var number in numbers)
+            var smallests = new List<int>();
+            while(smallests.Count < 3)
             {
-                if(!uniques.Contains(number))
-                    uniques.Add(number);
+                var min = numbers[0];
+                foreach(var number in numbers)
+                {
+                    if(number < min)
+                        min = number;
+                }
+                smallests.Add(min);
+
+                numbers.Remove(min);
             }
-            Console.WriteLine("Unique numbers: ");
-            foreach(var number in uniques)
+            Console.WriteLine("The 3 smallest numbers are: ");
+            foreach(var number in smallests)
                 Console.WriteLine(number);
-            
+
+
         }
     }
 }
