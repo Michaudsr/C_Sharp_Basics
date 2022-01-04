0b05226e09add8475e3d8bc20e7bc787fbf5402c
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2022-01-03 22:27:59 -0500

    [StrBuilder] using a method called StringBuilder to create new strings and munipulate them

---
+++ StrBuilder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace StrBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+            
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.Append('-', 10);
+            builder.Append('-', 10);
+            // Console.WriteLine(builder);
+
+            builder.Replace('-', '+');
+            // Console.WriteLine(builder);
+
+            builder.Remove(0, 10);   
+            // Console.WriteLine(builder);
+
+            builder.Insert(0, new string('-', 10));
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First Char: " + builder[0]);
+
+
+        }
+    }
+}
