f806d8663483d56272aa5ad7553fe447d1220e10
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2022-01-04 21:37:15 -0500

    [excercises] program that askes user to input numbers seperated by a hyphen and determines if they are unique or duplicate

--- excercises/Program.cs
+++ excercises/Program.cs
@@ -3,32 +3,58 @@ using System.Collections.Generic;
 
 namespace excercises
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             
-            Console.WriteLine("Enter a few numbers separated by a hyphen ");
-            var input = Console.ReadLine();
+            // Console.WriteLine("Enter a few numbers separated by a hyphen ");
+            // var input = Console.ReadLine();
             
+            // var numbers = new List<int>();
+            // foreach (var number in input.Split('-'))
+            //     numbers.Add(Convert.ToInt32(number));                
+
+            // numbers.Sort();
+
+            // var isConsecutive = true;
+            // for (var i = 1; i < numbers.Count; i++)
+            // {
+            //     if (numbers[i] != numbers[i - 1] + 1)
+            //     {
+            //         isConsecutive = false;
+            //         break;
+            //     }
+            // }
+
+            // var message = isConsecutive ? "Consecutive" : "Not Consecutive";
+            // Console.WriteLine(message);
+
+            Console.Write("Enter a few numbers (eg 1-2-3-4): ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+                return;
+
             var numbers = new List<int>();
             foreach (var number in input.Split('-'))
-                numbers.Add(Convert.ToInt32(number));                
+                numbers.Add(Convert.ToInt32(numbers));
 
-            numbers.Sort();
-
-            var isConsecutive = true;
-            for (var i = 1; i < numbers.Count; i++)
+            var isUnique = new List<int>();
+            var includesDuplicates = false;
+            foreach (var number in numbers)
             {
-                if (numbers[i] != numbers[i - 1] + 1)
+                if (!isUnique.Contains(number))
+                    isUnique.Add(number);
+                else 
                 {
-                    isConsecutive = false;
+                    includesDuplicates = true;
                     break;
                 }
-            }
 
-            var message = isConsecutive ? "Consecutive" : "Not Consecutive";
-            Console.WriteLine(message);
+            }
+            if (includesDuplicates)
+                Console.WriteLine("Duplicate");
 
         }
     }
